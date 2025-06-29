4cdf2f43d57217dda4f209978b756736d56e5e47
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-29 20:02:34 +0530

    D

--- 01_create_table_customers.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE customers (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(100),
-    country VARCHAR(100),
-    score INT
-);
--- 02_create_table_orders.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT NOT NULL,
-    order_date DATE,
-    sales INT
-);
