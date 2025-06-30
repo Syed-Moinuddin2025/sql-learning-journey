1ff9051745e79f16318fd7dfec6b9c39c4c96e33
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-30 23:25:10 +0530

    Travel Project Report

---
+++ travel project -1/travel_project_reports.sql
@@ -0,0 +1,6 @@
+-- 🔹 1. Total Number of Customers
+SELECT COUNT(*) AS TotalCustomers FROM Customers;
+-- 🔹 2. Total Number of Trips
+SELECT COUNT(*) AS TotalTrips FROM Trips;
+-- 🔹 3. Total Number of Bookings
+SELECT COUNT(*) AS TotalBookings FROM Bookings;
