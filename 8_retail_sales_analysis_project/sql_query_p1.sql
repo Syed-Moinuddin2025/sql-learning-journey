51f9fe916c52b80816df7fd48ff9d1bd70504ed6
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-07-01 12:38:06 +0530

    retail_sales

--- 8_retail_sales_analysis_project/sql_query_p1.sql
+++ 8_retail_sales_analysis_project/sql_query_p1.sql
@@ -19,8 +19,8 @@ CREATE TABLE retail_sales
                 total_sale FLOAT
             );
 
-SELECT * FROM retail_sales
-LIMIT 10
+SELECT TOP 5 * FROM retail_sales
+
 
 
     
@@ -36,12 +36,14 @@ WHERE transactions_id IS NULL
 SELECT * FROM retail_sales
 WHERE sale_date IS NULL
 
+EXEC sp_rename 'retail_sales.quantiy', 'quantity', 'COLUMN';
+
 SELECT * FROM retail_sales
 WHERE sale_time IS NULL
 
 SELECT * FROM retail_sales
 WHERE 
-    transaction_id IS NULL
+    transactions_id IS NULL
     OR
     sale_date IS NULL
     OR 
@@ -60,7 +62,7 @@ WHERE
 -- 
 DELETE FROM retail_sales
 WHERE 
-    transaction_id IS NULL
+    transactions_id IS NULL
     OR
     sale_date IS NULL
     OR 
@@ -121,7 +123,7 @@ FROM retail_sales
 WHERE 
     category = 'Clothing'
     AND 
-    TO_CHAR(sale_date, 'YYYY-MM') = '2022-11'
+    FORMAT(sale_date, 'yyyy-MM') = '2022-11'
     AND
     quantity >= 4
 
@@ -133,7 +135,7 @@ SELECT
     SUM(total_sale) as net_sale,
     COUNT(*) as total_orders
 FROM retail_sales
-GROUP BY 1
+GROUP BY category
 
 -- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
 
@@ -166,32 +168,35 @@ ORDER BY 1
 -- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
 
 SELECT 
-       year,
-       month,
+    year,
+    month,
     avg_sale
 FROM 
-(    
-SELECT 
-    EXTRACT(YEAR FROM sale_date) as year,
-    EXTRACT(MONTH FROM sale_date) as month,
-    AVG(total_sale) as avg_sale,
-    RANK() OVER(PARTITION BY EXTRACT(YEAR FROM sale_date) ORDER BY AVG(total_sale) DESC) as rank
-FROM retail_sales
-GROUP BY 1, 2
-) as t1
-WHERE rank = 1
+(
+    SELECT 
+        YEAR(sale_date) AS year,
+        MONTH(sale_date) AS month,
+        AVG(total_sale) AS avg_sale,
+        RANK() OVER (
+            PARTITION BY YEAR(sale_date)
+            ORDER BY AVG(total_sale) DESC
+        ) AS rank
+    FROM retail_sales
+    GROUP BY YEAR(sale_date), MONTH(sale_date)
+) AS t1
+WHERE rank = 1;
+
     
 -- ORDER BY 1, 3 DESC
 
 -- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
 
-SELECT 
+SELECT TOP 5
     customer_id,
-    SUM(total_sale) as total_sales
+    SUM(total_sale) AS total_sales
 FROM retail_sales
-GROUP BY 1
-ORDER BY 2 DESC
-LIMIT 5
+GROUP BY customer_id
+ORDER BY total_sales DESC;
 
 -- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
 
@@ -200,28 +205,25 @@ SELECT
     category,    
     COUNT(DISTINCT customer_id) as cnt_unique_cs
 FROM retail_sales
-GROUP BY category
-
-
+GROUP BY category;
 
 -- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <12, Afternoon Between 12 & 17, Evening >17)
 
-WITH hourly_sale
-AS
-(
-SELECT *,
-    CASE
-        WHEN EXTRACT(HOUR FROM sale_time) < 12 THEN 'Morning'
-        WHEN EXTRACT(HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
-        ELSE 'Evening'
-    END as shift
-FROM retail_sales
+WITH hourly_sale AS (
+    SELECT *,
+        CASE
+            WHEN DATEPART(HOUR, sale_time) < 12 THEN 'Morning'
+            WHEN DATEPART(HOUR, sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+            ELSE 'Evening'
+        END AS shift
+    FROM retail_sales
 )
 SELECT 
     shift,
-    COUNT(*) as total_orders    
+    COUNT(*) AS total_orders    
 FROM hourly_sale
-GROUP BY shift
+GROUP BY shift;
 
 -- End of project
+
 
